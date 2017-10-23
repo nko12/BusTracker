489a3cbf723b79d07c863970077fce3d4d8e27f8
Nkosi Dean <nko12@comcast.net>
2017-10-22 23:10:08 -0400

    Make it work.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
 import './App.css';
+// import * as busmap from './Components/BusMap/BusMap';
+import GoogleMap from 'google-map-react';
 
-const logo = require('./logo.svg');
+// const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <GoogleMap 
+        defaultZoom={11} 
+        defaultCenter={{lat: 59.95, lng: 30.33}}
+      />
     );
   }
 }
