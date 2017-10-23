17e5d20a794656315d030ce1eef9e49c7ff53a0b
Jarrett Wendt <jarrettwendt@jBook.local>
2017-10-23 09:55:06 -0400

    maps fixed to right side

--- client/src/components/BusMap.tsx
+++ client/src/components/BusMap.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import GoogleMap from 'google-map-react';
+import './../styles/BusMap.css';
 
 export class BusMap extends React.Component {
 	render() {
 		return (
-			<GoogleMap
-				defaultZoom={15} 
-				defaultCenter={{lat: 40.71, lng: -74}}
-			/>
+			<div className='BusMap'>
+				<GoogleMap
+					defaultZoom={15} 
+					defaultCenter={{lat: 40.71, lng: -74}}
+				/>
+			</div>
 		);
 	}
 }
