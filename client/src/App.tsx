5819f285972e3513077efc55bd5d28c1ddc7bd92
Jarrett Wendt <jarrettwendt@jBook.local>
2017-10-23 21:24:54 -0400

    a good looking, semi-functional demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import BusMap from './components/BusMap';
 import SideBar from './components/SideBar';
-// import {NavigationDrawer} from 'react-md';
+// import {TabsContainer, Tabs, Tab} from 'react-md';
 import './App.css';
 
 // const logo = require('./logo.svg');
 
-export class App extends React.Component {
+export class App extends React.Component {	
 	render() {
 		return (
 			<div>
@@ -20,20 +20,6 @@ export class App extends React.Component {
 					/>
 				</div>
 			</div>
-			// <NavigationDrawer
-			// 	drawerTitle='react-md with CRA'
-			// 	toolbarTitle='Welcome to react-md'
-			// >
-			// 	<div className='App'>
-			// 		<header className='App-header'>
-			// 			<img src={logo} className='App-logo' alt='logo' />
-			// 			<h1 className='App-title'>Welcome to React</h1>
-			// 		</header>
-			// 		<p className='App-intro'>
-			// 			To get started, edit <code>src/App.tsx</code> and save to reload.
-			// 		</p>
-			// 	</div>
-			// </NavigationDrawer>
 		);
 	}
 }
--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,30 +1,39 @@
 import * as React from 'react';
-import {Tabs, Tab} from 'react-md';
-// import * as ReactMD from 'react-md';
+import {TabsContainer, Tabs, Tab, TextField, Button} from 'react-md';
 
 export class SideBar extends React.Component<any, any> {
 	render() {
 		return (
-			// <ReactMD.TextField
-			// 	placeholder='wawawewa'
-			// 	type='text'
-			// 	helpText='green is not a creative colour'
-			// 	// helpOnFocus='true'
-			// 	defaultValue='40.7128' // -74.0060
-			// >
-			// </ReactMD.TextField>
 			<div>
-				<Tabs
-					activeTab={2}
-					tabId={1}
+				<TabsContainer
+					className='tabs__page-layout'
+					panelClassName='md-grid'
 				>
-					<Tab>Starks</Tab>
-					<Tab>Lannisters</Tab>
-					<Tab>Targaryens</Tab>
-				</Tabs>
-				<section>
-					<div>Content for this tab: TODO </div>
-				</section>
+					<Tabs tabId='phone-stuffs'>
+						<Tab label='Starks'>
+							<h1>Winter is Coming</h1>
+							<TextField
+								label='Latitude A'
+								defaultValue='41.337716'
+							/>
+							<TextField
+								label='Longitude A'
+								defaultValue='-74.35912'
+							/>
+							<TextField
+								label='Latitude B'
+								defaultValue='40.7588528'
+							/>
+							<TextField
+								label='Longitude B'
+								defaultValue='-73.9852625'
+							/>
+							<Button flat>Get Directions</Button>
+						</Tab>
+						<Tab label='Targaryans'><h1>Fire and Blood</h1></Tab>
+						<Tab label='Lannisters'><h1>A Lannister Always Pays His Debts</h1></Tab>
+					</Tabs>
+				</TabsContainer>
 			</div>
 		);
 	}
