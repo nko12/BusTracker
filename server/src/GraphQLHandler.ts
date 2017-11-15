43279e2f6931d67dc6051372a5a39c6860b1f324
Nkosi Dean <nko12@comcast.net>
2017-11-14 21:15:31 -0500

    Static Login and Register Working
    Got login and register graphql endpoints working to return the same static data.

--- server/src/GraphQLHandler.ts
+++ server/src/GraphQLHandler.ts
@@ -7,8 +7,7 @@ import * as models from './Models';
 /**
  * Represents the object sent to the client with GraphQL to represent the user
  */
-interface GraphQLUser {
-  id?: string;
+class GraphQLUser extends models.User {
   error?: string;
 }
 
@@ -40,29 +39,45 @@ class GraphQLHandler {
       this.schema = buildSchema(`
         type User {
           id: String
-          name: String
+          error: String
+          username: String
         }
 
         type Query {
           login(username: String, passwordHash: String): User
         }
+
+        type Mutation {
+          register(username: String, passwordHash: String): User
+        }
       `);
     }
+
     public login(data: LoginQueryData): GraphQLUser {
+      const user: GraphQLUser = new GraphQLUser();
+      user.username = 'Login Test User';
+      return user;
+    }
+
+    public register(data: LoginQueryData): GraphQLUser {
+      const user: GraphQLUser = new GraphQLUser();
+      user.username = 'Register Test User';
+      return user;
+    }
+
+    public login2(data: LoginQueryData): Promise<GraphQLUser> {
       console.log("GraphQL login attempt: {username=" + data.username + ",password=" + data.passwordHash + "}");
-      this.server.storage.loginUser(data.username, data.passwordHash).then((res: TypedResult<models.User>) => {
-        console.log("Got data");
-        //if (res.success && res.data) {
-          return {
-            id: "testid" // res.data.id
-          };
-      //  return {
-      //    error: "error" // res.message
-      //  };
-      }).catch((error) => {
-        console.log("Error in GraphQL login: ", error);
+
+      return new Promise<GraphQLUser>((resolve, reject) => {
+
+        this.server.storage.loginUser(data.username, data.passwordHash).then((res: TypedResult<models.User>) => {
+          console.log("Got data successfully");
+          resolve(new GraphQLUser());
+        }).catch((error) => {
+          console.log("Error in GraphQL login: ", error);
+          reject(new GraphQLUser());
+        });
       });
-      return {id: "test"};
     }
 }
 
