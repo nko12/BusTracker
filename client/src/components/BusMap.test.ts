b429f8d0d99897c76763b93af6eeb16ef8e8b384
Nkosi Dean <nko12@comcast.net>
2017-11-14 19:12:35 -0500

    Sync Only Commit

---
+++ client/src/components/BusMap.test.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import BusMap  from '../components/BusMap'
+
+describe('BusMap', () => {
+
+    it('should always have a GoogleMap root element.', () => {
+        
+    });
+});
