8aeeddbb66bb9bb2c6f1d6101abf7ccea3a0b94b
Nkosi Dean <nko12@comcast.net>
2017-11-16 20:44:00 -0500

    Setup A Basic Test Demonstrating Use of Enzyme
    Added a basic test that runs and passes. This should resolve #11 and allow component testing for the client.

--- client/src/components/BusMap.test.tsx
+++ client/src/components/BusMap.test.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
-import BusMap  from '../components/BusMap'
+import { mount, ReactWrapper } from 'enzyme';
+import GoogleMap from 'google-map-react';
+
+import {BusMapProps, BusMapState}  from '../components/BusMap';
+import BusMap from '../components/BusMap';
 
 describe('BusMap', () => {
 
-    it('should always have a GoogleMap root element.', () => {
+    // Since the Google Map is a pretty heavy component, we'll want to do as much testing as possible on a single
+    // instance of the BusMap.
+    let busMapWrapper: ReactWrapper<BusMapProps, BusMapState>;
+    let props: BusMapProps;
+    
+    beforeAll(() => {
+
+        // Create some bus map properties.
+        props = {
+            pointA: {lat: 41.337716, lng: -74.35912},
+            pointB: {lat: 40.7588528, lng: -73.9852625},
+            zoom: 5
+        }
+        // Create a busmap object to test on.
         
+        busMapWrapper = mount(<BusMap pointA={props.pointA} pointB={props.pointB} zoom={5} />);
+    });
+
+    it('should render a GoogleMap element.', () => {
+        
+        expect(busMapWrapper.find(GoogleMap).length).toEqual(1);
     });
 });
