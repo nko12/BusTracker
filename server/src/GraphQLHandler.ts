6ba563a33327892d673a632613f762521e7c2bf5
Alex Coleman <alexrcoleman13@gmail.com>
2017-11-19 16:37:50 -0500

    Added GraphQL support for editing favorites, and setup the schema for the rest of the functions

--- server/src/GraphQLHandler.ts
+++ server/src/GraphQLHandler.ts
@@ -3,9 +3,21 @@ var { buildSchema } = require('graphql');
 import { Result, TypedResult } from './Result';
 import * as models from './Models';
 
+
+/**
+ * Represents the basic error interface for GraphQL responses
+ */
 interface IError {
   error?: string;
 }
+
+/**
+ * Represents the object sent to the client for GraphQL to represent only a failure/success response
+ */
+class GraphQLBasicResponse implements IError {
+  error?: string;
+}
+
 /**
  * Represents the object sent to the client with GraphQL to represent the user
  */
@@ -41,6 +53,13 @@ interface LoginQueryData {
 interface IdQueryData {
   id: string;
 }
+/**
+ * Represents the object sent from the client for an id update (route or stop favorite update)
+ */
+interface IdArrayMutationData {
+  userId: string
+  objectIds: Array<string>;
+}
 
 /**
  * Represents the GraphQL handling component of the server.
@@ -71,6 +90,10 @@ class GraphQLHandler {
     public init(): void {
       this.schema = buildSchema(`
         interface IError {
+          error: String
+        }
+
+        type BasicResponse implements IError {
           error: String
         }
 
@@ -107,9 +130,18 @@ class GraphQLHandler {
 
         type Mutation {
           register(username: String, passwordHash: String): User
+          toggleAdminRights(grantingId: String, targetId: String, adminStatus: Boolean): BasicResponse
+          editFavoriteBusStopIDs(userId: String, objectIds: [String]): BasicResponse
+          editFavoriteRouteIDs(userId: String, objectIds: [String]): BasicResponse
+          addNewRoute(userId: String, routeData: Route): BasicResponse
+          addNewBusStop(userId: String, stopData: BusStop): BasicResponse
+          removeRoute(userId: String, routeId: String): BasicResponse
+          removeBusStop(userId: String, stopId: String): BasicResponse
         }
       `);
     }
+
+    /**    Queries     */
 
     /**
      * Handler that deals with the login query.
@@ -119,29 +151,8 @@ class GraphQLHandler {
       let user: GraphQLUser = new GraphQLUser();
 
       const result = await this.server.storage.loginUser(data.username, data.passwordHash);
-      if (!result.success)
-      {
+      if (!result.success) {
           // Failed to login the user.
-          user.error = result.message;
-          return user;
-      }
-
-      user = <GraphQLUser> result.data;
-      return user;
-    }
-
-    /**
-     * Handler that deals with the register mutation.
-     * @param data The register data (same form as login data) passed from the client.
-     */
-    public async register(data: LoginQueryData): Promise<GraphQLUser> {
-
-      let user: GraphQLUser = new GraphQLUser();
-
-      const result = await this.server.storage.registerUser(data.username, data.passwordHash);
-      if (!result.success)
-      {
-          // Failed to register the user.
           user.error = result.message;
           return user;
       }
@@ -159,9 +170,7 @@ class GraphQLHandler {
       let route: GraphQLRoute = new GraphQLRoute();
 
       const result = await this.server.storage.getRoute(data.id);
-      if (!result.success)
-      {
-          // Failed to register the user.
+      if (!result.success) {
           route.error = result.message;
           return route;
       }
@@ -179,15 +188,64 @@ class GraphQLHandler {
       let stop: GraphQLBusStop = new GraphQLBusStop();
 
       const result = await this.server.storage.getBusStop(data.id);
-      if (!result.success)
-      {
-          // Failed to register the user.
+      if (!result.success) {
           stop.error = result.message;
           return stop;
       }
 
       stop = <GraphQLBusStop> result.data;
       return stop;
+    }
+
+
+    /*     Mutations    */
+
+    /**
+     * Handler that deals with the register mutation.
+     * @param data The register data (same form as login data) passed from the client.
+     */
+    public async register(data: LoginQueryData): Promise<GraphQLUser> {
+
+      let user: GraphQLUser = new GraphQLUser();
+
+      const result = await this.server.storage.registerUser(data.username, data.passwordHash);
+      if (!result.success) {
+          user.error = result.message;
+          return user;
+      }
+
+      user = <GraphQLUser> result.data;
+      return user;
+    }
+
+    /**
+     * Handler that deals with the editFavoriteBusStopIDs mutation.
+     * @param data The bus stop ids and user data passed from the client.
+     */
+    public async editFavoriteBusStopIDs(data: IdArrayMutationData): Promise<GraphQLBasicResponse> {
+
+      let response: GraphQLBasicResponse = new GraphQLBasicResponse();
+
+      const result = await this.server.storage.editFavoriteBusStopIDs(data.userId, data.objectIds);
+      if (!result.success) {
+          response.error = result.message;
+      }
+      return response;
+    }
+
+    /**
+     * Handler that deals with the editFavoriteRouteIDs mutation.
+     * @param data The route ids and user data passed from the client.
+     */
+    public async editFavoriteRouteIDs(data: IdArrayMutationData): Promise<GraphQLBasicResponse> {
+
+      let response: GraphQLBasicResponse = new GraphQLBasicResponse();
+
+      const result = await this.server.storage.editFavoriteRouteIDs(data.userId, data.objectIds);
+      if (!result.success) {
+          response.error = result.message;
+      }
+      return response;
     }
 }
 
